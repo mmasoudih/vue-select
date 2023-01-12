5ca3582c49880b3e5fdbd27d3ea5b33d2f244ee7
Mohammad Masoudi <mohammad.masoudi59@gmail.com>
2023-01-12 18:02:39 +0330

    add custom-vue-select

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,38 @@
       target-key="value"
       :default-selected="[25]"
     />
+
+    <custom-vue-select
+      id="month"
+      v-model="selectedCycle"
+      :options="cycleData"
+      based-on="cycle_name"
+      target-key="cycle_detail_id"
+    >
+      <template #option="{ item, click }">
+        <div
+          class="d-flex align-items-center justify-content-between"
+          style="cursor: pointer"
+        >
+          <span @click="click">{{ item.cycle_name }}</span>
+        </div>
+      </template>
+    </custom-vue-select>
   </div>
 </template>
 
 <script>
 import CustomVueMultiSelect from "./components/CustomVueMultiSelect.vue";
-
+import CustomVueSelect from "./components/CustomVueSelect.vue";
 export default {
   name: "App",
   components: {
     CustomVueMultiSelect,
+    CustomVueSelect
   },
   data() {
     return {
-      selectedOption: '',
+      selectedOption: "",
       cycleDetail: [
         { text: "فروردین-1401", value: 13 },
         { text: "اردیبهشت-1401", value: 14 },
@@ -37,6 +55,23 @@ export default {
         { text: "اسفند-1401", value: 24 },
         { text: "فروردین-1402", value: 25 },
       ],
+
+      cycleData: [
+        { cycle_detail_id: 13, cycle_name: "فروردین-1401", is_current: 0 },
+        { cycle_detail_id: 14, cycle_name: "اردیبهشت-1401", is_current: 0 },
+        { cycle_detail_id: 15, cycle_name: "خرداد-1401", is_current: 0 },
+        { cycle_detail_id: 16, cycle_name: "تیر-1401", is_current: 0 },
+        { cycle_detail_id: 17, cycle_name: "مرداد-1401", is_current: 0 },
+        { cycle_detail_id: 18, cycle_name: "شهریور-1401", is_current: 0 },
+        { cycle_detail_id: 19, cycle_name: "مهر-1401", is_current: 0 },
+        { cycle_detail_id: 20, cycle_name: "آبان-1401", is_current: 0 },
+        { cycle_detail_id: 21, cycle_name: "آذر-1401", is_current: 0 },
+        { cycle_detail_id: 22, cycle_name: "دی-1401", is_current: 1 },
+        { cycle_detail_id: 23, cycle_name: "بهمن-1401", is_current: 0 },
+        { cycle_detail_id: 24, cycle_name: "اسفند-1401", is_current: 0 },
+        { cycle_detail_id: 25, cycle_name: "فروردین-1402", is_current: 0 },
+      ],
+      selectedCycle:''
     };
   },
 };
---
+++ src/components/CustomVueSelect.vue
@@ -0,0 +1,230 @@
+<!-- eslint-disable no-undef -->
+<!-- eslint-disable no-nested-ternary -->
+<!-- eslint-disable space-before-blocks -->
+<!-- eslint-disable object-curly-newline -->
+<!-- eslint-disable vue/singleline-html-element-content-newline -->
+<!-- eslint-disable vue/max-attributes-per-line -->
+<!-- eslint-disable vue/html-self-closing -->
+<!-- eslint-disable vue/html-indent -->
+
+<template>
+	<div
+		:id="id"
+		v-click-outside="closeDropdown"
+		class="vue-select"
+		:class="{ 'is-open': isOpen }"
+		:tabindex="tabindex"
+	>
+		<div class="select-body" @click="isOpen = !isOpen">
+			<div class="selected-value text-nowrap" :title="getOptionValue(selected)">
+				{{ getOptionValue(selected) }}
+				<span v-if="!selected && placeholder" class="placeholder">
+					{{ placeholder }}
+				</span>
+			</div>
+			<div class="indicator-icon">
+				<img
+					
+					alt="bill-debt"
+					draggable="false"
+					width="23"
+				/>
+			</div>
+		</div>
+		<transition name="fade">
+			<div v-show="isOpen" class="dropdown">
+				<slot name="header" />
+				<div v-if="!options.length" class="d-flex justify-content-center p-2">
+					<!-- <puff-loading :style="{ color: 'var(--primary)' }" /> -->
+				</div>
+				<ul>
+					<li
+						v-for="(item, index) in options"
+						:key="index"
+						@click="preventDefaultClickEvent ? null : onChange(item)"
+					>
+						<slot
+							name="option"
+							:item="item"
+							:value="getOptionValue(item)"
+							:click="onChange.bind(this, item)"
+						>
+							{{ getOptionValue(item) }}
+						</slot>
+					</li>
+				</ul>
+				<slot name="footer" />
+			</div>
+		</transition>
+	</div>
+</template>
+
+<script>
+// import PuffLoading from "../svg-icons/PuffLoading.vue";
+
+export default {
+	components: {
+		// PuffLoading,
+	},
+	props: {
+		id: {
+			type: String,
+			required: false,
+		},
+		value: {
+			type: [Object, Array, String, Number],
+			required: false,
+			default: null,
+		},
+		options: {
+			type: Array,
+			required: true,
+		},
+		basedOn: {
+			type: String,
+			default: null,
+		},
+		targetKey: {
+			type: String,
+			required: false,
+		},
+		preventDefaultClickEvent: {
+			type: Boolean,
+			default: false,
+		},
+		tabindex: {
+			type: Number,
+			required: false,
+			default: 0,
+		},
+		placeholder: {
+			type: String,
+			default: "",
+		},
+	},
+	data() {
+		return {
+			selected: this.$props.value || null,
+			isOpen: false,
+		};
+	},
+	computed: {
+		hasOptionSlot() {
+			return !!this.$slots.option;
+		},
+	},
+	mounted() {
+		this.$emit("input", this.selected);
+	},
+	methods: {
+		getOptionValue(anOption) {
+			if (anOption === Object(anOption)) return anOption[this.basedOn];
+			return anOption;
+		},
+		onChange(value) {
+			this.selected = value;
+
+			if (this.targetKey) {
+				this.$emit("input", this.selected[this.targetKey]);
+			} else {
+				this.$emit("input", this.selected);
+			}
+
+			this.isOpen = false;
+		},
+		closeDropdown() {
+			this.isOpen = false;
+		},
+		textTrim(text, length) {
+			if (typeof text === "string") {
+				return text.length > length ? `... ${text.substring(0, length)}` : text;
+			}
+			return text;
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+// @import "@core/scss/base/bootstrap-extended/_variables.scss";
+
+$border-radius: 10px;
+$black: #000;
+$cvs-box-shadow: 0 4px 15px 0 rgba($black, 0.3);
+$primary: #fff;.vue-select {
+	margin-top: 0.5rem;
+	width: 100%;
+	position: relative;
+}
+
+.vue-select > .select-body {
+	direction: rtl;
+	display: flex;
+	flex-direction: row-reverse;
+	justify-content: space-between;
+	border: solid 1px rgba(0, 0, 0, 0.2);
+	border-radius: $border-radius;
+	align-items: center;
+	transition: box-shadow 200ms linear;
+}
+
+.vue-select.is-open .select-body {
+	box-shadow: $cvs-box-shadow;
+}
+
+.vue-select .selected-value {
+	height: 3rem;
+	padding: 0.9rem 1rem;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	direction: ltr;
+}
+
+.placeholder {
+	color: rgba(0, 0, 0, 0.6);
+}
+
+.vue-select.is-open .indicator-icon {
+	transform: rotate(180deg);
+}
+.vue-select .indicator-icon {
+	transition: transform 200ms linear;
+	padding: 0 0.5rem;
+}
+
+.vue-select > .dropdown {
+	z-index: 10;
+	padding: 1rem;
+	box-shadow: $cvs-box-shadow;
+	position: absolute;
+	background-color: white;
+	border-radius: $border-radius;
+	left: 0;
+	right: 0;
+	max-height: 25vh;
+	overflow: auto;
+}
+
+.vue-select ul li {
+	width: 100%;
+	padding: 0 0 0.7rem 0;
+	border-bottom: solid 0.01rem rgba(0, 0, 0, 0.1);
+	margin-bottom: 0.7rem;
+	color: rgba(0, 0, 0, 0.8);
+	font-size: 1.1rem;
+}
+
+.vue-select ul li:last-child {
+	border: none;
+	margin: 0;
+}
+
+ul li {
+	list-style: none;
+}
+
+ul {
+	margin: 0;
+	padding: 0;
+}
+</style>
